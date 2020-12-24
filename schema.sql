d9b62f9f5b0c2df2e64804da64c6ba8749ec8c66
DarkGeekMS <mohamedshawky911@gmail.com>
2020-12-25 00:37:03 +0200

    change the schema primary keys

--- schema.sql
+++ schema.sql
@@ -3,7 +3,8 @@ CREATE DATABASE Disasters_DB;
 USE Disasters_DB;
 
 CREATE TABLE Disaster (
-  name varchar(40) PRIMARY KEY,
+  id int PRIMARY KEY AUTO_INCREMENT,
+  name varchar(40) UNIQUE,
   possible_causes text,
   precautions text,
   no_of_prev_occur int DEFAULT 0
@@ -11,18 +12,18 @@ CREATE TABLE Disaster (
 
 CREATE TABLE Incident (
   id int PRIMARY KEY AUTO_INCREMENT,
-  eco_loss int DEFAULT 0,
   year int NOT NULL,
   month int NOT NULL,
   day int NOT NULL,
   description text,
+  eco_loss int DEFAULT 0,
   location varchar(60) NOT NULL,
   name varchar(40),
-  type varchar(40)
+  type int
 );
 
 CREATE TABLE Person (
-  ssn varchar(14) PRIMARY KEY,
+  id int PRIMARY KEY AUTO_INCREMENT,
   name varchar(40) NOT NULL,
   age int,
   gender int,
@@ -30,19 +31,19 @@ CREATE TABLE Person (
 );
 
 CREATE TABLE Casualty (
-  ssn varchar(14) PRIMARY KEY,
+  id int PRIMARY KEY,
   deg_of_loss varchar(10) NOT NULL
 );
 
 CREATE TABLE Government_Representative (
-  ssn varchar(14) PRIMARY KEY,
+  id int PRIMARY KEY,
   username varchar(40) NOT NULL,
   password varchar(60) NOT NULL,
   data_of_join datetime DEFAULT (CURRENT_TIMESTAMP())
 );
 
 CREATE TABLE Citizen (
-  ssn varchar(14) PRIMARY KEY,
+  id int PRIMARY KEY,
   username varchar(40) NOT NULL,
   password varchar(60) NOT NULL,
   data_of_join datetime DEFAULT (CURRENT_TIMESTAMP()),
@@ -50,34 +51,34 @@ CREATE TABLE Citizen (
 );
 
 CREATE TABLE Report (
-  report_id int PRIMARY KEY AUTO_INCREMENT,
+  id int PRIMARY KEY AUTO_INCREMENT,
   content text NOT NULL,
   report_date datetime DEFAULT (CURRENT_TIMESTAMP()),
   incident_id int,
-  govn_ssn varchar(14),
-  citizen_ssn varchar(14)
+  govn_id int,
+  citizen_id int
 );
 
 CREATE TABLE Casualty_Incident (
   incident_id int,
-  casualty_ssn varchar(14),
-  primary key (incident_id, casualty_ssn)
+  casualty_id int,
+  PRIMARY KEY (incident_id, casualty_id)
 );
 
-ALTER TABLE Incident ADD FOREIGN KEY (type) REFERENCES Disaster (name) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE Incident ADD FOREIGN KEY (type) REFERENCES Disaster (id) ON DELETE SET NULL ON UPDATE CASCADE;
 
-ALTER TABLE Casualty ADD FOREIGN KEY (ssn) REFERENCES Person (ssn) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE Casualty ADD FOREIGN KEY (id) REFERENCES Person (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE Government_Representative ADD FOREIGN KEY (ssn) REFERENCES Person (ssn) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE Government_Representative ADD FOREIGN KEY (id) REFERENCES Person (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE Citizen ADD FOREIGN KEY (ssn) REFERENCES Person (ssn) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE Citizen ADD FOREIGN KEY (id) REFERENCES Person (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE Report ADD FOREIGN KEY (incident_id) REFERENCES Incident (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE Report ADD FOREIGN KEY (govn_ssn) REFERENCES Government_Representative (ssn) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE Report ADD FOREIGN KEY (govn_id) REFERENCES Government_Representative (id) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE Report ADD FOREIGN KEY (citizen_ssn) REFERENCES Citizen (ssn) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE Report ADD FOREIGN KEY (citizen_id) REFERENCES Citizen (id) ON DELETE SET NULL ON UPDATE CASCADE;
 
 ALTER TABLE Casualty_Incident ADD FOREIGN KEY (incident_id) REFERENCES Incident (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE Casualty_Incident ADD FOREIGN KEY (casualty_ssn) REFERENCES Casualty (ssn) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE Casualty_Incident ADD FOREIGN KEY (casualty_id) REFERENCES Casualty (id) ON DELETE CASCADE ON UPDATE CASCADE;
